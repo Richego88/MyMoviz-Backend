873091022a0742877d549577bbb08dde8c705c72
Richard <rchegwin88@gmail.com>
2023-11-15 16:13:38 +0100

    done

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+var express = require("express");
+var router = express.Router();
+
+const apiKey = process.env.TMDB_API_KEY;
+
+router.get("/movies", (req, res) => {
+  fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}`)
+    .then((response) => response.json())
+    .then((data) => {
+      res.json({ movies: data.results });
+    });
+});
+
+module.exports = router;
